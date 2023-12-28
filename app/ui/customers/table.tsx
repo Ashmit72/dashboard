5a39b39f7931d9cf0766757d02bbf83bf3fdf968
Ashmit/72 <ashmitbastolayt@gmail.com>
2023-12-28 11:38:19 +0545

    second commit

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,8 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import { lusitana } from '@/app/ui/font';
 import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
