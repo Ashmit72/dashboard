08e8fc38cfd2790580c5ada770b5b58fb8eeef33
Ashmit/72 <ashmitbastolayt@gmail.com>
2023-12-28 20:20:24 +0545

    Update Information

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,11 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
           <p className={` ${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <strong>Welcome to Dashboard.</strong> This app is made up of by the help of 
               Next.js Learn Course
-            </a>
             , brought to you by Vercel.
           </p>
+          <p>Use "user@nextmail.com" for Email and "123456" for Password</p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
